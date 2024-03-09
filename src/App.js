a02f20f949112689ed5dcee5b6a33d22684838fa
Yavuz Samet Kan <yssk.personal+github@gmail.com>
2024-03-09 20:57:08 +0300

    --Deployment settings have been made

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {MainRoutes} from "./routes/CustomRoutes";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
-    const BASE_NAME = '/'
+    const BASE_NAME = '/reactjs-basics-formic-operations'
   return (
       <BrowserRouter basename={BASE_NAME}>
         <Routes>
